799e74c245b4adba697b96368a7d4a428f167af5
eunice-shaji <A00322510@mycambrian.ca>
2025-11-06 22:39:38 -0500

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,6 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        MainPage = new MainPage(); // Or MaainPage if that's your correct class name
+        MainPage = new MainPage(); 
     }
 }
